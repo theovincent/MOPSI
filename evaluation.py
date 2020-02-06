2155368d2e79e0cb5a8dd6b831e5df86c209b426
Pia Chancerel <pia.chancerel@eleves.enpc.fr>
2020-02-06 17:30:08 +0100

    creation matrice des sshape

--- evaluation.py
+++ evaluation.py
@@ -13,22 +13,20 @@ positionnements est une matrice 2D  de taille : longueur_rangees * nb_rangees do
 historique est une matrice 2D de taille : nb_ref * nb_ref donnant les probas de chaque
     couple de références d'être commandée.
 """
+
 from pathlib import Path
 import numpy as np
 from alea import alea
 from generateur import extraction_commande
 
 
-# --- S-shape --- #
-def sshape_2(entrepot, commande, longueur_rangees, nb_rangees):
+
+def sshape(longueur_rangees, nb_rangees, commande):
     """
     Calcul le coût d'aller chercher la commande.
 
     Parametres:
-        entrepot (array de taille nb_ref) : entrepot[refi] indique le numéro de la
-        rangée de la référence d'indice refi.
-
-        commande (Array de taille (nb_ref, nb_ref)): matrice des probabilités des commandes.
+        commande (array de taille 2) : commande[0] correspond à la position [rangee, casier] de la 1e référence à aller chercher, commande[1] à la position de la 2e
 
         longueur_rangees (Entier): la longueur des rangées dans l'entrepôt.
 
@@ -37,14 +35,20 @@ def sshape_2(entrepot, commande, longueur_rangees, nb_rangees):
     Return:
         temps (Entier): le nombre de casse que met le robot à aller les deux références
 
-    >>> sshape_2([0, 1], [0, 1], 1, 2)
-    10
+    >>> sshape(3, 3, [[0, 2], [1, 0]])
+    14
+    >>> sshape(3, 3, [[1, 0], [1, 1]])
+    12
+    >>> sshape(3, 3, [[0, 2], [2, 0]])
+    18
     """
     # Il y a les couloirs à prendre en compte
     largeur_entrepot = nb_rangees * 2 + 1
     longueur_entrepot = longueur_rangees + 2
-    rangee1 = entrepot[commande[0]]
-    rangee2 = entrepot[commande[1]]
+    rangee1 = commande[0][0]
+    casier1 = commande[0][1]
+    rangee2 = commande[1][0]
+    casier2 = commande[1][1]
 
     # L'entrée est au milieu de l'entrepot. largeur_entrepot est impaire.
     entree = largeur_entrepot // 2
@@ -69,99 +73,101 @@ def sshape_2(entrepot, commande, longueur_rangees, nb_rangees):
     return temps
 
 
-"""
-def sshape_qlc(nb_rangees, longueur_rangees, positionnements, commande):
 
-    largeur_entrepot = nb_rangees*2 + 1
-    longueur_entrepot = longueur_rangees + 2
-    positionnements = positionnements + 1 # les rangees sont numerotees à partir de 0
-
-    rangees_a_visiter = []
-    for ref in commande:
-        if (positionnements[ref]) not in rangees_a_visiter:
-            rangees_a_visiter.append(positionnements[ref])
-    rangees_a_visiter.sort()
-
-    temps = 1 # on rentre dans l'entrepot
-    x_milieu = (largeur_entrepot + 1)/2 # largeur_entrepot est impair
-    x_robot = x_milieu
-
-    for r in rangees_a_visiter:
-        temps += abs(1+2*r- x_robot) + (longueur_entrepot - 1)
-        x_robot = 1+2*r
-
-    # on calcule le temps restants pour sortir de l'entrepot
-    if (len(rangees_a_visiter)%2 != 0): # si on se retrouve au fond de l'entrepot
-        temps += longueur_entrepot - 1
-    temps += abs(x_robot - x_milieu) + 1
-
-    return temps
-"""
-
-
-# --- Evaluation une configuration donnée --- #
-def positionnement_to_entrepot(positionnement):
+def evalue_entrepot(longueur_rangees, nb_rangees):
     """
-    Permet de créer un entrepot à partir de positionnement
+    Evalue le temps de récupération pour chaque paire de positions.
 
     Parametres:
-        positionnement (Array de taille (longueur_rangees, nb_rangees)): la position
-        des références dans l'entrepôt.
+        longueur_rangees (Entier): la longueur des rangées dans l'entrepôt.
+
+        nb_rangees (Entier): le nombre de rangées dans l'entrepôt
 
     Return:
-        entrepot (array de taille nb_ref) : entrepot[refi] indique le numéro de la
-        rangée de la référence d'indice refi.
+        temps (Array de taille (nb_ref, nb_ref)): temps[i, j] du temps que le robot met à chercher 2 objets en position i et j dans l'entrepôt (attention, les positions sont codées en 1D : [rangee, casier] = [rangee + casier*nb_rangees])
 
-    >>> list(positionnement_to_entrepot(np.array([[0, 1], [3, 2]])))
-    [0.0, 1.0, 1.0, 0.0]
+    >>> evalue_entrepot(1, 4)
+    array([[10., 10., 14., 18.], [10., 6., 10., 14.], [14., 10., 10., 14.], [18., 14., 14., 14.]])
+    >>> evalue_entrepot(2, 2)
+    array([[8., 12., 8., 12.], [12., 12., 12., 12.], [8., 12., 8., 12.], [12., 12., 12., 12.]])
     """
-    longeur_rangees = len(positionnement)
-    nb_rangees = len(positionnement[0])
-    entrepot = np.zeros(nb_rangees * longeur_rangees)
+    nb_places = longueur_rangees*nb_rangees
+    temps = np.zeros((nb_places, nb_places))
+    for rangee1 in range(nb_rangees):
+        for casier1 in range(longueur_rangees):
+            for rangee2 in range(nb_rangees):
+                for casier2 in range(longueur_rangees):
+                    commande = [[rangee1, casier1], [rangee2, casier2]]
+                    place1 = rangee1 + casier1*nb_rangees
+                    place2 = rangee2 + casier2*nb_rangees
+                    temps[place1, place2] = sshape(longueur_rangees, nb_rangees, commande)
+    return temps
 
-    for profondeur in range(longeur_rangees):
-        for index_rangee in range(nb_rangees):
-            entrepot[int(positionnement[profondeur, index_rangee])] = index_rangee
+
+
+def inverse_positionnement(positionnement):
+    """
+    Attribue à chaque référence sa position, à partir de la matrice qui associe à chaque position sa référence
+
+    Parametres:
+        positionnement (Array de taille (longueur_rangees, nb_rangees)): positionnement[i, j] renvoie la référence placée au casier j de la rangée i
+
+    Return:
+        entrepot (array de taille nb_ref) : entrepot[refi] = [rangee, casier] qui indique la rangée et le casier où est placé refi
+
+    >>> inverse_positionnement(np.array([[3, 1], [0, 2]]))
+    [[0.0, 1.0], [1.0, 0.0], [1.0, 1.0], [0.0, 0.0]]
+    """
+    longueur_rangees = len(positionnement)
+    nb_rangees = len(positionnement[0])
+    nb_ref = nb_rangees * longueur_rangees
+    entrepot = np.zeros((nb_ref, 2))
+
+    for rangee in range(nb_rangees):
+        for casier in range(longueur_rangees):
+            entrepot[positionnement[casier, rangee], 0] = rangee
+            entrepot[positionnement[casier, rangee], 1] = casier
 
     return entrepot
 
 
-def evalue(positionnement, proba):
+
+def evalue_position(positionnement, temps_entrepot, proba):
     """
-    Evalue la valeur d'un positionnement, connaîssant
-    les probabilités des commandes
+    Evalue le temps moyen d'un positionnement.
 
     Parametres:
         positionnement (Array de taille (longueur_rangees, nb_rangees): la position
         des références dans l'entrepôt.
 
-        praba (Array de taille (nb_ref, nb_ref)): matrice des probabilités des commandes.
+        temps_entrepot (Array de taille (nb_ref, nb_ref)): temps[i, j] du temps que le robot met à chercher 2 objets en position i et j dans l'entrepôt (attention, les positions sont codées en 1D : [rangee, casier] = [rangee + casier*nb_rangees])
+
+        proba (Array de taille (nb_ref, nb_ref)): matrice des probabilités des commandes.
 
     Return:
-        esperance (Réelle): l'esperance du temps que le robot met à chercher une commande.
+        esperance (entier): le temps moyen mis pour collecter une commande
 
-    >>> proba = np.array([[0, 1], [1, 0]])
-    >>> evalue(np.array([[1, 0]]), proba)
-    10.0
+    >>> evalue_position(np.array([[1, 3], [0, 2]]), evalue_entrepot(2, 2), np.array([[0.0, 0.2, 0.4, 0.0], [0.2, 0, 0.1, 0.1], [0.4, 0.1, 0.0, 0.2], [0.0, 0.1, 0.3, 0.0]]))
+    11.2
     """
     nb_rangees = len(positionnement[0])
-    longeur_rangees = len(positionnement)
-    nb_ref = nb_rangees * longeur_rangees
-    entrepot = positionnement_to_entrepot(positionnement)
+    longueur_rangees = len(positionnement)
+    nb_ref = nb_rangees * longueur_rangees
+    position_ref = inverse_positionnement(positionnement)
 
-    # -- Calculons l'espérance du positionnement -- #
     esperance = 0
     for ref1 in range(nb_ref):
-        # [ref1, ref2] et [ref2, ref1] sont les mêmes commandes
         for ref2 in range(ref1 + 1, nb_ref):
-            temps_commande = sshape_2(entrepot, [ref1, ref2], longeur_rangees, nb_rangees)
+            position_ref1 = int(position_ref[ref1, 0] + position_ref[ref1, 1]*nb_rangees)
+            position_ref2 = int(position_ref[ref2, 0] + position_ref[ref2, 1]*nb_rangees)
+            temps_commande = temps_entrepot[position_ref1, position_ref2]
             esperance += temps_commande * proba[ref1, ref2]
 
     return esperance
 
 
+
 if __name__ == "__main__":
-    # -- Doc tests -- #
     import doctest
     doctest.testmod()
 
@@ -172,9 +178,9 @@ if __name__ == "__main__":
 
     # Définition arbitraire de la longueur et du nombre des rangées
     LONGUEUR_RANGEES = 3
-    NB_RANGEES = NB_REF // 3
+    NB_RANGEES = NB_REF // LONGUEUR_RANGEES
 
     # Chargement d'un positionnement aléatoire
     POS_ALEA = alea(LONGUEUR_RANGEES, NB_RANGEES)
 
-    print(evalue(POS_ALEA, PROBA))
+    #print(evalue(POS_ALEA, PROBA))
