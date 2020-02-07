50e2600ed3869ef467d78b3cc111d550ccfe949c
Théo VINCENT <theo.vincent@eleves.enpc.fr>
2020-02-07 12:15:23 +0100

    evalution doc test

--- evaluation.py
+++ evaluation.py
@@ -20,7 +20,6 @@ from alea import alea
 from generateur import extraction_commande
 
 
-
 def sshape(longueur_rangees, nb_rangees, position1, position2):
     """
     Calcul le coût d'aller chercher la commande.
@@ -31,6 +30,7 @@ def sshape(longueur_rangees, nb_rangees, position1, position2):
         nb_rangees (Entier): le nombre de rangées dans l'entrepôt.
 
         position1 (liste de 2 entiers) : [rangee1, casier1] correspondant au 1e emplacement auquel se rendre
+            si casier1 = 0, c'est que l'objet est au fond de la rangée
 
         position2 (liste de 2 entiers) : [rangee2, casier2] correspondant au 2e emplacement auquel se rendre
 
@@ -58,10 +58,10 @@ def sshape(longueur_rangees, nb_rangees, position1, position2):
     # L'entrée est au milieu de l'entrepot. largeur_entrepot est impaire.
     # On passe à droite des rangées
     entree = largeur_entrepot // 2 + 1
-    allee_rangee1 = 3 + 2*rangee1
-    allee_rangee2 = 3 + 2*rangee2
+    allee_rangee1 = 3 + 2 * rangee1
+    allee_rangee2 = 3 + 2 * rangee2
 
-    if rangee1==rangee2 :
+    if rangee1 == rangee2:
         # le temps de récupération le plus rapide si les 2 réf sont dans la même rangée est :
         temps = 2*(1 + abs(allee_rangee1 - entree) + (longueur_rangees - min(casier1, casier2)))
         return temps
@@ -75,7 +75,6 @@ def sshape(longueur_rangees, nb_rangees, position1, position2):
     return min(temps1, temps2)
 
 
-
 def evalue_entrepot(longueur_rangees, nb_rangees):
     """
     Evalue le temps de récupération pour chaque paire de positions.
@@ -86,7 +85,9 @@ def evalue_entrepot(longueur_rangees, nb_rangees):
         nb_rangees (Entier): le nombre de rangées dans l'entrepôt
 
     Return:
-        temps (Array de taille (nb_ref, nb_ref)): temps[i, j] du temps que le robot met à chercher 2 objets en position i et j dans l'entrepôt (attention, les positions sont codées en 1D : [rangee, casier] = [rangee + casier*nb_rangees])
+        temps (Array de taille (nb_ref, nb_ref)): temps[i, j] du temps que le robot met à chercher 2 objets en position
+            i et j dans l'entrepôt (attention, les positions sont codées en 1D):
+            [rangee, casier] = [rangee + casier*nb_rangees])
 
     >>> evalue_entrepot(1, 4)
     array([[10., 10., 14., 18.], [10., 6., 10., 14.], [14., 10., 10., 14.], [18., 14., 14., 14.]])
@@ -100,13 +101,12 @@ def evalue_entrepot(longueur_rangees, nb_rangees):
             for rangee2 in range(nb_rangees):
                 for casier2 in range(longueur_rangees):
                     position1 = [rangee1, casier1]
-                    position1 = [rangee2, casier2]
-                    place1 = rangee1 + casier1*nb_rangees
-                    place2 = rangee2 + casier2*nb_rangees
+                    position2 = [rangee2, casier2]
+                    place1 = rangee1 + casier1 * nb_rangees
+                    place2 = rangee2 + casier2 * nb_rangees
                     temps[place1, place2] = sshape(longueur_rangees, nb_rangees, position1, position2)
 
     return temps
-
 
 
 def inverse_positionnement(positionnement):
@@ -114,10 +114,12 @@ def inverse_positionnement(positionnement):
     Attribue à chaque référence sa position, à partir de la matrice qui associe à chaque position sa référence
 
     Parametres:
-        positionnement (Array de taille (longueur_rangees, nb_rangees)): positionnement[i, j] renvoie la référence placée au casier j de la rangée i
+        positionnement (Array de taille (longueur_rangees, nb_rangees)): positionnement[i, j] renvoie la référence
+        placée au casier j de la rangée i
 
     Return:
-        entrepot (array de taille nb_ref) : entrepot[refi] = [rangee, casier] qui indique la rangée et le casier où est placé refi
+        entrepot (array de taille nb_ref) : entrepot[refi] = [rangee, casier] qui indique la rangée et le casier où est
+        placé refi
 
     >>> inverse_positionnement(np.array([[3, 1], [0, 2]]))
     [[0.0, 1.0], [1.0, 0.0], [1.0, 1.0], [0.0, 0.0]]
@@ -135,7 +137,6 @@ def inverse_positionnement(positionnement):
     return entrepot
 
 
-
 def evalue_position(positionnement, temps_entrepot, proba):
     """
     Evalue le temps moyen d'un positionnement.
@@ -144,14 +145,17 @@ def evalue_position(positionnement, temps_entrepot, proba):
         positionnement (Array de taille (longueur_rangees, nb_rangees): la position
         des références dans l'entrepôt.
 
-        temps_entrepot (Array de taille (nb_ref, nb_ref)): temps[i, j] du temps que le robot met à chercher 2 objets en position i et j dans l'entrepôt (attention, les positions sont codées en 1D : [rangee, casier] = [rangee + casier*nb_rangees])
+        temps_entrepot (Array de taille (nb_ref, nb_ref)): temps[i, j] du temps que le robot met à chercher 2 objets en
+            position i et j dans l'entrepôt (attention, les positions sont codées en 1D :
+            [rangee, casier] = [rangee + casier * nb_rangees])
 
         proba (Array de taille (nb_ref, nb_ref)): matrice des probabilités des commandes.
 
     Return:
         esperance (entier): le temps moyen mis pour collecter une commande
 
-    >>> evalue_position(np.array([[1, 3], [0, 2]]), evalue_entrepot(2, 2), np.array([[0.0, 0.2, 0.4, 0.0], [0.2, 0, 0.1, 0.1], [0.4, 0.1, 0.0, 0.2], [0.0, 0.1, 0.3, 0.0]]))
+    >>> proba = np.array([[0.0, 0.2, 0.4, 0.0], [0.2, 0, 0.1, 0.1], [0.4, 0.1, 0.0, 0.2], [0.0, 0.1, 0.3, 0.0]])
+    >>> evalue_position(np.array([[1, 3], [0, 2]]), evalue_entrepot(2, 2), proba)
     11.2
     """
     nb_rangees = len(positionnement[0])
@@ -170,21 +174,20 @@ def evalue_position(positionnement, temps_entrepot, proba):
     return esperance
 
 
-
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
 
-   #   # Paramètre pour charger les probabilités des commandes
+    # --- Paramètre pour charger les probabilités des commandes --- #
     PATH_COMMANDE = Path("test.txt")
     PROBA = extraction_commande(PATH_COMMANDE)
     NB_REF = len(PROBA)
 
-   #   # Définition arbitraire de la longueur et du nombre des rangées
+    # --- Définition arbitraire de la longueur et du nombre des rangées --- #
     LONGUEUR_RANGEES = 3
     NB_RANGEES = NB_REF // LONGUEUR_RANGEES
 
-   #   # Chargement d'un positionnement aléatoire
+    # --- Chargement d'un positionnement aléatoire --- #
     POS_ALEA = alea(LONGUEUR_RANGEES, NB_RANGEES)
 
-    print(evalue(POS_ALEA, PROBA))
+    # print(evalue(POS_ALEA, PROBA))
